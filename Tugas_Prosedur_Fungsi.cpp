da84891a5cea911a08053ccdc87883124776028c
sandy ramadhan <32477879+sandyramadhan@users.noreply.github.com>
2017-10-17 22:34:48 +0700

    Add files via upload

---
+++ Tugas_Prosedur_Fungsi.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int maks (int a, int b);
+
+int main() {
+
+	int a,b;
+ 
+
+ 	cout << "masukan nilai a : " ;
+	cin >> a;
+
+	cout << "masukan nilai b : " ;
+	cin >> b;
+
+ cout<< "nilai terbesar adalah: " << maks (a,b);
+ return 0;
+	
+}
+
+int maks (int a, int b) {
+	if (a > b) {
+		return a;
+	}
+	else if (a < b) {
+		return b;
+	}
+	else {
+		return 0;
+	}
+}
